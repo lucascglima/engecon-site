86ece3892ea2b1cc4dcad61c652648f4566ae38c
Lucas Lima <lucascglima@gmail.com>
2023-09-09 18:27:07 -0300

    fix: text-porfolio-services

--- src/components/Main/Portfolio2/structural-reinforcement.jsx
+++ src/components/Main/Portfolio2/structural-reinforcement.jsx
@@ -59,7 +59,7 @@ const Portfolio = () => {
                   <a className="imago wow">
                     <img src={image.url} alt="image" />
                     <div className="item-img-overlay dark-blue-bg text-white justify-center">
-                      <span className="text-center see-more see-more-services text-light fz-20">
+                      <span className="text-center see-more see-more-services text-light fz-16">
                         {image.details}
                       </span>
                       <i className="fas fa-arrow-right icon text-light fz-20"></i>
